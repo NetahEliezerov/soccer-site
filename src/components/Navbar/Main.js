abaa79e2cb70661aec731324650b9a18acec49d8
adaminspaceship <adam.eliezerov@gmail.com>
2021-11-23 23:38:00 +0200

    add user icon to sidebar

--- src/components/Navbar/Main.js
+++ src/components/Navbar/Main.js
@@ -4,12 +4,13 @@ import './Main.css';
 const pages = [
     {
         nameOfPage: "Statistics",
-        icon: './assets/graph.png',
-        url: '#statistics'
+        icon: './assets/statistics.svg',
+        url: '#statistics',
+        isSelected: true
     },
     {
         nameOfPage: "Events",
-        icon: './assets/notification-status.png',
+        icon: './assets/events.svg',
         url: '#events'
     }
 ]
@@ -18,14 +19,17 @@ function MainNavbarComponent() {
     return (
         <div>
             <ul className="Nav">
+                <li>
+                    <div className="userIcon">F</div>
+                </li>
                 {pages.map((page) => <li>
-                    <button onClick={() => window.location.href = page.url} className="navbarButton activeNavbarBtn">
+                    <button onClick={() => window.location.href = page.url} className={`navbarButton ${page.isSelected ? "activeNavbarBtn" : ""}`}>
                         <img src={page.icon} /><br />
                         <span className="navbarItemTxt">{page.nameOfPage}</span>
                     </button>
                 </li>)}
             </ul>
-            
+
         </div>
     )
 }
